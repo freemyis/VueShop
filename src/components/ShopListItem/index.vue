2a2e346733ade3907dd63140b5d4db86b3063013
redking <2078592182@qq.com>
2020-04-14 11:56:35 +0800

    afternoon commit

--- src/components/ShopListItem/index.vue
+++ src/components/ShopListItem/index.vue
@@ -1,15 +1,46 @@
 <template>
     <div>
-        <h1>ShopListItem page</h1>
+        <h1>{{message}}</h1>
     </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment(state) {
+            state.count++;
+        }
+    }
+});
+
+store.commit('increment');
+console.log(store.state.count);
+
     export default {
-        name: 'ShopListItem'
+        name: 'ShopListItem',
+        data() {
+            return {
+                message: 'Hello World.'
+            }
+        },
+        computed: {
+            count() {
+                return store.state.count;
+            }
+        },
     }
 </script>
 
 <style scoped>
-
+    h1 {
+        margin-top: 6em;
+    }
 </style>
